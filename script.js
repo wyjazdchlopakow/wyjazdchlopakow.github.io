158678ca027587bc97f817d2b136950b371336d7
wyjazdchlopakow <140624903+wyjazdchlopakow@users.noreply.github.com>
2023-08-02 15:05:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const stoliceList = document.getElementById("stoliceList");
+const odwiedzoneStolice = ["Toruń", "Wrocław", "Lublin", "Kraków", "Warszawa", "Opole", "Poznań"];
 
 stoliceList.addEventListener("click", (event) => {
   const clickedLi = event.target;
   clickedLi.classList.toggle("visited");
 });
+
+// Odfiltrowanie nieodwiedzonych stolic i dodanie klasy "visited" tylko dla odwiedzonych
+const liElements = stoliceList.getElementsByTagName("li");
+for (const li of liElements) {
+  const stolica = li.textContent.trim();
+  if (odwiedzoneStolice.includes(stolica)) {
+    li.classList.add("visited");
+  }
+}
