97b4a5f1f5ee769692915ca7968120a1638dff9c
wyjazdchlopakow <140624903+wyjazdchlopakow@users.noreply.github.com>
2023-08-02 15:04:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,6 @@
-const wheel = document.getElementById("wheel");
-const spinBtn = document.getElementById("spin-btn");
-const result = document.getElementById("result");
+const stoliceList = document.getElementById("stoliceList");
 
-let spinning = false;
-let selectedItem;
-
-spinBtn.addEventListener("click", () => {
-    if (!spinning) {
-        spinning = true;
-        selectedItem = null;
-        const spinDuration = 3000; // 3 seconds
-        const degrees = 1080 + Math.floor(Math.random() * 1080); // Spin 3-6 full rotations
-        wheel.style.transition = `transform ${spinDuration}ms ease-out`;
-        wheel.style.transform = `rotate(${degrees}deg)`;
-        setTimeout(() => {
-            wheel.style.transition = "none";
-            spinning = false;
-            showResult();
-        }, spinDuration);
-    }
+stoliceList.addEventListener("click", (event) => {
+  const clickedLi = event.target;
+  clickedLi.classList.toggle("visited");
 });
-
-function showResult() {
-    if (selectedItem === null) {
-        const items = document.querySelectorAll(".wheel-item");
-        const randomIndex = Math.floor(Math.random() * items.length);
-        selectedItem = items[randomIndex].textContent;
-    }
-
-    result.textContent = `Selected: ${selectedItem}`;
-}
